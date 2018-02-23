023a49ef6be12b6461084ff1400b282b62f0c8af
Edward Sujono <edwardsujono81@gmail.com>
2018-02-24 00:23:49 +0800

    update backend url

--- backend/product/views.py
+++ backend/product/views.py
@@ -5,5 +5,11 @@ import pysolr
 # Create your views here.
 
 
-def search_product(request, product_name):
-    return JsonResponse({'success': product_name})
+def search_product(request):
+
+    list_mock_return = {'list_product': []}
+
+    for i in range(10):
+        list_mock_return.get('list_product').append({"product_name": "test_%s" % i})
+
+    return JsonResponse(list_mock_return)
