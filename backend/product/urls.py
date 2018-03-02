24cc3157d320e770cdba5e0b11f8f2d6a9f157b4
Edward Sujono <edwardsujono81@gmail.com>
2018-03-02 17:52:15 +0800

    update the new endpoint

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^search_product/$', views.search_product, name='search_product'),
+    url(r'^get_detail_item/$', views.get_detail_item, name='get_detail_item'),
+    url(r'^get_list_item/$', views.get_list_item, name='get_list_item'),
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from haystack.query import SearchQuerySet
 from django.views.decorators.csrf import csrf_exempt
-from product.models import ShopeeProducts
+from product.models import ShopeeProducts, AmazonProducts, LazadaProducts
 import json
 
 
@@ -22,3 +22,64 @@ def search_product(request):
         list_json_return.get('list_product').append({'product_name': result.product_name})
 
     return JsonResponse(list_json_return)
+
+
+@csrf_exempt
+def get_detail_item(request):
+
+    if request.method == "OPTIONS":
+        return JsonResponse({"success": True})
+
+    get_data = request.GET
+
+    product_id = get_data.get("product_id")
+    product_type = get_data.get("shop")
+
+    result = None
+
+    if product_type == "shopee":
+        result = ShopeeProducts.objects.get(product_id=int(product_id)).get_serialize()
+    elif product_type == "amazon":
+        result = AmazonProducts.objects.get(product_id=int(product_id)).get_serialize()
+    elif product_type == "lazada":
+        result = LazadaProducts.objects.get(product_id=int(product_id)).get_serialize()
+
+    return JsonResponse({'success': True, 'product': result})
+
+
+@csrf_exempt
+def get_list_item(request):
+
+    if request.method == "OPTIONS":
+        return JsonResponse({"success": True})
+
+    get_data = request.GET
+
+    product_name = get_data.get("product_name")
+    offset = int(get_data.get("offset"))
+    limit = int(get_data.get("limit"))
+
+    results = SearchQuerySet().all().filter(text=product_name)
+    list_json_return = {'list_product': []}
+
+    cnt = 0
+
+    for result in results:
+
+        if offset <= cnt < offset+limit:
+            list_json_return.get('list_product').append(
+                {
+                    'product_name': result.product_name,
+                    'product_id': result.product_id,
+                    'product_link': result.product_link,
+                    'product_description': result.product_description,
+                    'original_price': result.original_price,
+                    'current_price': result.current_price,
+                    'image_link': result.image_link
+                }
+            )
+
+        cnt += 1
+
+    return JsonResponse({"list_product": list_json_return, "total": len(results)})
+
