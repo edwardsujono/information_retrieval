c50c86c38d9fbf2fa8de1ab961ff1a1253b4dd91
Edward Sujono <edwardsujono81@gmail.com>
2018-03-03 14:33:03 +0800

    add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'cron_service',
     'product',
-    'haystack'
+    'haystack',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
